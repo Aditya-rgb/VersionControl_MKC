a86568a07af603a80f04b1acf87d1092e6875ae5
Aditya vakharia <adityavakharia@gmail.com>
2024-06-22 19:04:15 +0530

    Aditya's modification to UAT branch

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 print("Aditya 1st commit to this app.py file")
+
+
+
+print("***********************************START*****************************************")
+
+print("Locally redundant storage
+Locally redundant storage (LRS) replicates your storage account three times within a single data center in the primary region. LRS provides at least 99.999999999% (11 nines) durability of objects over a given year.
+
+LRS is the lowest-cost redundancy option and offers the least durability compared to other options. LRS protects your data against server rack and drive failures. However, if a disaster such as fire or flooding occurs within the data center, all replicas of a storage account using LRS may be lost or unrecoverable. To mitigate this risk, Microsoft recommends using zone-redundant storage (ZRS), geo-redundant storage (GRS), or geo-zone-redundant storage (GZRS).")
+
+
+print("***********************************END***********************************************")
--- index.py
+++ index.py
@@ -1,2 +1,14 @@
 print("This is the index file")
 
+
+
+print("*************************START*********************************************)
+
+print("Redundancy in the primary region
+Data in an Azure Storage account is always replicated three times in the primary region. Azure Storage offers two options for how your data is replicated in the primary region:
+
+Locally redundant storage (LRS) copies your data synchronously three times within a single physical location in the primary region. LRS is the least expensive replication option, but isn't recommended for applications requiring high availability or durability.
+Zone-redundant storage (ZRS) copies your data synchronously across three Azure availability zones in the primary region. For applications requiring high availability, Microsoft recommends using ZRS in the primary region, and also replicating to a secondary region.")
+
+
+print("*********************END**************************************************")
