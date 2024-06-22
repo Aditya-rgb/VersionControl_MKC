50718d5a12575b87f3fcd41a194f0267f979f04f
Aditya vakharia <adityavakharia@gmail.com>
2024-06-22 18:27:13 +0530

    Aditya's second commit to his feature branch

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
 print("Aditya 1st commit to this app.py file")
+print("For Azure managed disks:
+
+The following table illustrates the default and maximum limits of the number of resources per region per subscription. The limits remain the same irrespective of disks encrypted with either platform-managed keys or customer-managed keys. There is no limit for the number of Managed Disks, snapshots and images per resource group.")
